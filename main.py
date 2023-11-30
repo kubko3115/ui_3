674615ae17d0624a66c46bdddec5cc3c12a10ebc
Duncik815 <belisjakub815@gmail.com>
2023-11-30 17:06:43 +0100

    Merge branch 'main' of https://github.com/kubko3115/ui_3

--- main.py
+++ main.py
@@ -5,6 +5,10 @@ from enum import Enum
 import math
 import random
 import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt1
+import matplotlib.pyplot as plt3
+import matplotlib.pyplot as plt7
+import matplotlib.pyplot as plt15
 import time
 import numpy as np
 class Color(Enum):
@@ -19,6 +23,10 @@ class point:
     x = int
     y = int
     color = Color
+    color1 = Color
+    color3 = Color
+    color7 = Color
+    color15 = Color
     generatedcolor = Color
     distance = float
     def __init__(self, x, y, color):
@@ -53,6 +61,106 @@ def getMostFrequentColor(neighbours):
         elif n.color == Color.GREEN:
             green += 1
         elif n.color == Color.PURPLE:
+            purple += 1
+    if red > blue and red > green and red > purple:
+        return Color.RED
+    elif blue > red and blue > green and blue > purple:
+        return Color.BLUE
+    elif green > red and green > blue and green > purple:
+        return Color.GREEN
+    elif purple > red and purple > blue and purple > green:
+        return Color.PURPLE
+    else:
+        return Color.RED
+    
+def getMostFrequentColor1(neighbours):
+    red = 0
+    blue = 0
+    green = 0
+    purple = 0
+    for n in neighbours:
+        if n.color1 == Color.RED:
+            red += 1
+        elif n.color1 == Color.BLUE:
+            blue += 1
+        elif n.color1 == Color.GREEN:
+            green += 1
+        elif n.color1 == Color.PURPLE:
+            purple += 1
+    if red > blue and red > green and red > purple:
+        return Color.RED
+    elif blue > red and blue > green and blue > purple:
+        return Color.BLUE
+    elif green > red and green > blue and green > purple:
+        return Color.GREEN
+    elif purple > red and purple > blue and purple > green:
+        return Color.PURPLE
+    else:
+        return Color.RED
+    
+def getMostFrequentColor3(neighbours):
+    red = 0
+    blue = 0
+    green = 0
+    purple = 0
+    for n in neighbours:
+        if n.color3 == Color.RED:
+            red += 1
+        elif n.color3 == Color.BLUE:
+            blue += 1
+        elif n.color3 == Color.GREEN:
+            green += 1
+        elif n.color3 == Color.PURPLE:
+            purple += 1
+    if red > blue and red > green and red > purple:
+        return Color.RED
+    elif blue > red and blue > green and blue > purple:
+        return Color.BLUE
+    elif green > red and green > blue and green > purple:
+        return Color.GREEN
+    elif purple > red and purple > blue and purple > green:
+        return Color.PURPLE
+    else:
+        return Color.RED
+    
+def getMostFrequentColor7(neighbours):
+    red = 0
+    blue = 0
+    green = 0
+    purple = 0
+    for n in neighbours:
+        if n.color7 == Color.RED:
+            red += 1
+        elif n.color7 == Color.BLUE:
+            blue += 1
+        elif n.color7 == Color.GREEN:
+            green += 1
+        elif n.color7 == Color.PURPLE:
+            purple += 1
+    if red > blue and red > green and red > purple:
+        return Color.RED
+    elif blue > red and blue > green and blue > purple:
+        return Color.BLUE
+    elif green > red and green > blue and green > purple:
+        return Color.GREEN
+    elif purple > red and purple > blue and purple > green:
+        return Color.PURPLE
+    else:
+        return Color.RED
+    
+def getMostFrequentColor15(neighbours):
+    red = 0
+    blue = 0
+    green = 0
+    purple = 0
+    for n in neighbours:
+        if n.color15 == Color.RED:
+            red += 1
+        elif n.color15 == Color.BLUE:
+            blue += 1
+        elif n.color15 == Color.GREEN:
+            green += 1
+        elif n.color15 == Color.PURPLE:
             purple += 1
     if red > blue and red > green and red > purple:
         return Color.RED
@@ -105,9 +213,17 @@ def main():
             x = random.randint(-5000, 500)
             y = random.randint(-5000, 500)
         newpoint = point(x, y, Color.RED)
-        neighbours = getNearestNeighbours(k, newpoint, points)
         newpoint.generatedcolor = Color.RED
-        newpoint.color = getMostFrequentColor(neighbours)
+        # neighbours = getNearestNeighbours(k, newpoint, points)
+        # newpoint.color = getMostFrequentColor(neighbours)
+        neighbours = getNearestNeighbours(1, newpoint, points)
+        newpoint.color1 = neighbours[0].color1
+        neighbours = getNearestNeighbours(3, newpoint, points)
+        newpoint.color3 = getMostFrequentColor3(neighbours)
+        neighbours = getNearestNeighbours(7, newpoint, points)
+        newpoint.color7 = getMostFrequentColor7(neighbours)
+        neighbours = getNearestNeighbours(15, newpoint, points)
+        newpoint.color15 = getMostFrequentColor15(neighbours)
         # print(newpoint.color)
         points.append(newpoint)
         
@@ -118,9 +234,17 @@ def main():
             x = random.randint(-500,5000)
             y = random.randint(-5000, 500)
         newpoint = point(x, y, Color.GREEN)
-        neighbours = getNearestNeighbours(k, newpoint, points)
         newpoint.generatedcolor = Color.GREEN
-        newpoint.color = getMostFrequentColor(neighbours)
+        # neighbours = getNearestNeighbours(k, newpoint, points)
+        # newpoint.color = getMostFrequentColor(neighbours)
+        neighbours = getNearestNeighbours(1, newpoint, points)
+        newpoint.color1 = neighbours[0].color1
+        neighbours = getNearestNeighbours(3, newpoint, points)
+        newpoint.color3 = getMostFrequentColor3(neighbours)
+        neighbours = getNearestNeighbours(7, newpoint, points)
+        newpoint.color7 = getMostFrequentColor7(neighbours)
+        neighbours = getNearestNeighbours(15, newpoint, points)
+        newpoint.color15 = getMostFrequentColor15(neighbours)
         # print(newpoint.color)
         points.append(newpoint)
         
@@ -131,9 +255,17 @@ def main():
             x = random.randint(-5000, 500)
             y = random.randint(-500,5000)
         newpoint = point(x, y, Color.BLUE)
-        neighbours = getNearestNeighbours(k, newpoint, points)
         newpoint.generatedcolor = Color.BLUE
-        newpoint.color = getMostFrequentColor(neighbours)
+        # neighbours = getNearestNeighbours(k, newpoint, points)
+        # newpoint.color = getMostFrequentColor(neighbours)
+        neighbours = getNearestNeighbours(1, newpoint, points)
+        newpoint.color1 = neighbours[0].color
+        neighbours = getNearestNeighbours(3, newpoint, points)
+        newpoint.color3 = getMostFrequentColor3(neighbours)
+        neighbours = getNearestNeighbours(7, newpoint, points)
+        newpoint.color7 = getMostFrequentColor7(neighbours)
+        neighbours = getNearestNeighbours(15, newpoint, points)
+        newpoint.color15 = getMostFrequentColor15(neighbours)
         # print(newpoint.color)
         points.append(newpoint)
         
@@ -144,9 +276,17 @@ def main():
             x = random.randint(-500,5000)
             y = random.randint(-500,5000)
         newpoint = point(x, y, Color.PURPLE)
-        neighbours = getNearestNeighbours(k, newpoint, points)
         newpoint.generatedcolor = Color.PURPLE
-        newpoint.color = getMostFrequentColor(neighbours)
+        # neighbours = getNearestNeighbours(k, newpoint, points)
+        # newpoint.color = getMostFrequentColor(neighbours)
+        neighbours = getNearestNeighbours(1, newpoint, points)
+        newpoint.color1 = neighbours[0].color1
+        neighbours = getNearestNeighbours(3, newpoint, points)
+        newpoint.color3 = getMostFrequentColor3(neighbours)
+        neighbours = getNearestNeighbours(7, newpoint, points)
+        newpoint.color7 = getMostFrequentColor7(neighbours)
+        neighbours = getNearestNeighbours(15, newpoint, points)
+        newpoint.color15 = getMostFrequentColor15(neighbours)
         # print(newpoint.color)
         points.append(newpoint)
         if i % 1000 == 0:
@@ -156,24 +296,92 @@ def main():
     end_time = time.time()
     print("Time elapsed: ", end_time - start_time)
         
+    # for p in points:
+    #     if p.color == Color.BLUE:
+    #         plt.plot(p.x, p.y, 'bo')
+    #     elif p.color == Color.RED:
+    #         plt.plot(p.x, p.y, 'ro')
+    #     elif p.color == Color.GREEN:
+    #         plt.plot(p.x, p.y, 'go')
+    #     elif p.color == Color.PURPLE:
+    #         plt.plot(p.x, p.y, 'mo')
+    #     elif p.color == Color.YELLOW:
+    #         plt.plot(p.x, p.y, 'yo')
+    # plt.show()
+    
     for p in points:
-        if p.color == Color.BLUE:
-            plt.plot(p.x, p.y, 'bo')
-        elif p.color == Color.RED:
-            plt.plot(p.x, p.y, 'ro')
-        elif p.color == Color.GREEN:
-            plt.plot(p.x, p.y, 'go')
-        elif p.color == Color.PURPLE:
-            plt.plot(p.x, p.y, 'mo')
-        elif p.color == Color.YELLOW:
-            plt.plot(p.x, p.y, 'yo')
-    plt.show()
+        if p.color1 == Color.BLUE:
+            plt1.plot(p.x, p.y, 'bo')
+        elif p.color1 == Color.RED:
+            plt1.plot(p.x, p.y, 'ro')
+        elif p.color1 == Color.GREEN:
+            plt1.plot(p.x, p.y, 'go')
+        elif p.color1 == Color.PURPLE:
+            plt1.plot(p.x, p.y, 'mo')
+        elif p.color1 == Color.YELLOW:
+            plt1.plot(p.x, p.y, 'yo')
+    plt1.show()
+    
+    for p in points:
+        if p.color3 == Color.BLUE:
+            plt3.plot(p.x, p.y, 'bo')
+        elif p.color3 == Color.RED:
+            plt3.plot(p.x, p.y, 'ro')
+        elif p.color3 == Color.GREEN:
+            plt3.plot(p.x, p.y, 'go')
+        elif p.color3 == Color.PURPLE:
+            plt3.plot(p.x, p.y, 'mo')
+        elif p.color3 == Color.YELLOW:
+            plt3.plot(p.x, p.y, 'yo')
+    plt3.show()
+    
+    for p in points:
+        if p.color7 == Color.BLUE:
+            plt7.plot(p.x, p.y, 'bo')
+        elif p.color7 == Color.RED:
+            plt7.plot(p.x, p.y, 'ro')
+        elif p.color7 == Color.GREEN:
+            plt7.plot(p.x, p.y, 'go')
+        elif p.color7 == Color.PURPLE:
+            plt7.plot(p.x, p.y, 'mo')
+        elif p.color7 == Color.YELLOW:
+            plt7.plot(p.x, p.y, 'yo')
+    plt7.show()
+    
+    for p in points:
+        if p.color15 == Color.BLUE:
+            plt15.plot(p.x, p.y, 'bo')
+        elif p.color15 == Color.RED:
+            plt15.plot(p.x, p.y, 'ro')
+        elif p.color15 == Color.GREEN:
+            plt15.plot(p.x, p.y, 'go')
+        elif p.color15 == Color.PURPLE:
+            plt15.plot(p.x, p.y, 'mo')
+        elif p.color15 == Color.YELLOW:
+            plt15.plot(p.x, p.y, 'yo')
+    plt15.show()
+    
     
     z = 0
     for p in points:
-        if p.generatedcolor == p.color:
+        if p.generatedcolor == p.color1:
             z += 1
-    print("Accuracy: ", z/len(points)*100, "%")
+    print("Accuracy for k=1: ", z/len(points)*100, "%")
+    z = 0
+    for p in points:
+        if p.generatedcolor == p.color3:
+            z += 1
+    print("Accuracy for k=3: ", z/len(points)*100, "%")
+    z = 0
+    for p in points:
+        if p.generatedcolor == p.color7:
+            z += 1
+    print("Accuracy for k=7: ", z/len(points)*100, "%")
+    z = 0
+    for p in points:
+        if p.generatedcolor == p.color15:
+            z += 1
+    print("Accuracy for k=15: ", z/len(points)*100, "%")
             
     
 main()
